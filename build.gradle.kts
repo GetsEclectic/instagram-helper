f63976e61abb6d25f234fae7608e7175b5014578
Ben Solwitz <notalamer@gmail.com>
2019-07-25 16:15:18 -0400

    got test running

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.3.11"
+    kotlin("jvm") version "1.3.41"
 }
 
-group = "ben"
+group = "getseclectic"
 version = "1.0-SNAPSHOT"
 
 repositories {
@@ -14,17 +14,9 @@ repositories {
 dependencies {
     compile(kotlin("stdlib-jdk8"))
     compile("org.brunocvcunha.instagram4j:instagram4j:1.11")
-    testImplementation("com.nhaarman.mockitokotlin2:mockito-kotlin:2.1.0")
-    testImplementation("org.assertj:assertj-core:3.12.2")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.4.2")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:1.3.11")
-    testRuntime("org.junit.jupiter:junit-jupiter-engine:5.4.2")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.5.1")
 }
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
 }
-
-//test {
-//    useJUnitPlatform()
-//}
---
+++ src/test/kotlin/Instagram4KTest.kt
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+internal class Instagram4KTest {
+    @Test
+    fun `my first test`() {
+        assertEquals(1, 1, "should be equal")
+    }
+}
