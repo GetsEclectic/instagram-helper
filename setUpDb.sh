87c6c6626a6f66dc258d843abb19ce45a60501cd
Ben Solwitz <notalamer@gmail.com>
2019-08-22 13:58:10 -0400

    randomized host port in db setup

--- setUpDb.sh
+++ setUpDb.sh
@@ -4,6 +4,7 @@ set -x
 
 ADMIN_PW=$(cat /dev/urandom | LC_CTYPE=c tr -dc 'a-zA-Z0-9' | head -c16)
 USER_PW=$(cat /dev/urandom | LC_CTYPE=c tr -dc 'a-zA-Z0-9' | head -c16)
+HOST_PORT=$((32768 + RANDOM))
 
 cat > postgressetup.sql <<EOF
 create database instagram4k;
@@ -13,14 +14,14 @@ EOF
 
 docker build -t instagram4k_postgres .
 
-docker run --name instagram4k_db -e POSTGRES_PASSWORD="$ADMIN_PW" -d -p 38492:5432 -it instagram4k_postgres
+docker run --name instagram4k_db -e POSTGRES_PASSWORD="$ADMIN_PW" -d -p "$HOST_PORT":5432 -it instagram4k_postgres
 
 rm postgressetup.sql
 
 cat > dbconfig.properties <<EOF
 db.user=instagram4k_app
 db.password=$USER_PW
-db.url=jdbc:postgresql://localhost:38492/instagram4k
+db.url=jdbc:postgresql://localhost:$HOST_PORT/instagram4k
 EOF
 
 sleep 5s
